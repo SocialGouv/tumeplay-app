a9568a47a968a0891766c28e155e11448bb4cc50
R.P <romain.petiteville@celaneo.com>
2020-04-21 15:59:26 +0200

    Bumping version number

--- src/screens/CustomFooter.js
+++ src/screens/CustomFooter.js
@@ -99,7 +99,7 @@ export default function CustomFooter(props) {
           color: '#CCCCCC',
           marginTop: 15,
         }}>
-        Version Beta - 20200309.2
+        Version Beta - 20200421.1 - PREPROD
       </Text>
     </View>
   );

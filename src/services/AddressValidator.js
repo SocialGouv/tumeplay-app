843460e556c91ce2145f0c45539f9da84f1b0a4a
R.P <romain.petiteville@celaneo.com>
2021-03-05 17:03:40 +0100

    fix: Improve zipcode length

--- src/services/AddressValidator.js
+++ src/services/AddressValidator.js
@@ -1,6 +1,5 @@
 const AddressValidator = {
   allowedZipCodes: [
-    '77',
     '16',
     '17',
     '19',
@@ -10,10 +9,15 @@ const AddressValidator = {
     '40',
     '47',
     '64',
+    '75',
+    '77',
     '79',
     '86',
     '87',
-    '97',
+    '91',
+    '93',
+    '94',
+    '95',         
   ],
   zipCodeTest: /^[0-9]{5}$/,
   zipCodePartTest: /^[0-9]{2}$/,
